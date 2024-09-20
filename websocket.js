0d6dc2e4304e7684842840a329ea64b5fe08fb78
pupupu270 <lycee0@yahoo.co.jp>
2024-09-20 09:35:14 +0900

    Update websocket.js

--- websocket.js
+++ websocket.js
@@ -1,6 +1,6 @@
 
 let websocket =require("ws");
-let server = new websocket.Server({port:0});
+let server = new websocket.Server({port:3000});
 
 server.on("connection",(socket)=>{
 console.log("connection");
